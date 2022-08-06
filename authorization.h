61071ee24be0079567f700d310099777b573ab91
Vadim <Vadim32654@gmail.com>
2022-08-06 22:36:55 +0500

    Update authorization.h

--- authorization.h
+++ authorization.h
@@ -7,14 +7,14 @@
 #include "car_rent.h"
 
 
-map<string, string> accounts(); // Чтение данных из файла
+map<string, string> accounts(); // Reading data from a file
 
-bool correct_login(const string& log); // Проверка корректности введенного логина
+bool correct_login(const string& log); // Checking the correctness of the entered login
 
-void authorization(); // Авторизация в системе
+void authorization(); // Authorization in the system
 
-bool password_check(const string& pass1, const string& pass2); // Проверка корректности введенного пароля при создании аккаунта
+bool password_check(const string& pass1, const string& pass2); // Checking the correctness of the entered password when creating an account
 
-bool unique_login(const string& log); // Проверка уникальности логина
+bool unique_login(const string& log); // Checking the uniqueness of the login
 
-bool create_account(); // Создание аккаунта
+bool create_account(); // Creating an account
